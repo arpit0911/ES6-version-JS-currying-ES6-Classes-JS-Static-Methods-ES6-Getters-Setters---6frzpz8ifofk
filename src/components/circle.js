16bbf174b69bc3b07da1f9db3d43f32076bb0431
singharpit0911@gmail.com <singharpit0911@gmail.com>
2022-07-05 22:19:38 +0530

    classes created imported and exported

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
+import { Shape } from "./shape";
 
 // declare class
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
 
+  calculateArea() {
+    console.log("inside calculateArea in Circle");
+  }
+}
 
 // export class using module.exports
+export { Circle };
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape() {
+    console.log("inside drawShape in Shape ")
+  }
+  calculateArea() {
+    console.log("inside calculateArea in Shape ")
+
+  }
+}
 
 // export class using module.exports
+export {Shape}
